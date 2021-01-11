c7af8bd56d87afff33f6dab1c71c42c5cca86ee2
Rosie Tempest <rosietempest@gmail.com>
2021-01-10 21:44:51 -0800

    added the tag functionality

--- index.js
+++ index.js
@@ -5,30 +5,43 @@ const projects = [
         'name': 'D3 Scattter Chart',
         'url': 'https://rtempest.github.io/fcc_scatter_chart/',
         'img': 'fcc_scatter_chart_cropped.jpg',
-        'description': 'Challenge #2 of the freeCodeCamp Data Visualization Certificate'
+        'description': 'Challenge #2 of the freeCodeCamp Data Visualization Certificate',
+        'tags': ['d3.js', 'data-viz']
     },
     {
-        'name': 'project 2',
-        'url': 'http://www.google.com'
+        'name': 'Vancouver Street Trees',
+        'url': 'http://www.google.com',
+        'img': 'fcc_scatter_chart_cropped.jpg',
+        'description': 'An interactive web map of street trees in downtown Vancouver made with Leaflet.js',
+        'tags': ['leaflet', 'webmaps']
     },
     {
-        'name': 'project 3',
-        'url': 'http://www.google.com'
+        'name': 'Bar Chart',
+        'url': 'http://www.google.com',
+        'img': 'fcc_scatter_chart_cropped.jpg',
+        'description': 'A bar chart',
+        'tags': ['d3.js', 'data-viz']
     }
 ]
 
+const makeTags = function (tags) {
+    let result = ''
+    tags.forEach((tag) => result += `<span class='tag'>${tag}</span>`)
+    return result
+}
+
 projects.forEach((x) => {
-    container.innerHTML += `<div class="col">
-    <div class="card shadow-sm">
-    <a href=${x.url}>
-        <img src=${x.img} width="350" height="auto"></a>
-        <div class="card-body">
-        <h3>${x.name}</h3>
-            <p class="card-text">${x.description}</p>
-            <div class="d-flex justify-content-between align-items-center">
+    container.innerHTML += `
+    <div class="col">
+        <div class="card shadow-sm">
+            <a href=${x.url}>
+            <img src=${x.img} width="300" height="auto"></a>
+            <div class="card-body">
+                <h3>${x.name}</h3>
+                <p class="card-text">${x.description}</p>
+                ${makeTags(x.tags)}
             </div>
         </div>
-    </div>
-</div>`
+    </div>`
 
-})
+});
